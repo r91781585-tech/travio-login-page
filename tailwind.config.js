2e76ee52095770190830d71cb0bbbdf096198abe
r91781585-tech <r91781585@gmail.com>
2025-09-19 21:51:48 +0530

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'travio-yellow': '#F5E663',
+        'travio-gray': '#E5E5E5',
+      },
+    },
+  },
+  plugins: [],
+}
